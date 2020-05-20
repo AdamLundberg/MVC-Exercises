f01fc0cfc409cf8f02d7d435aaccc73ef51604fd
Adam <adamlundberg91@gmail.com>
2020-05-20 11:21:14 +0200

    Initial commit

---
+++ src/main/java/se/kalmar/adam/MVCExercises/MvcExercisesApplication.java
@@ -0,0 +1,13 @@
+package se.kalmar.adam.MVCExercises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcExercisesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcExercisesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/kalmar/adam/MVCExercises/MvcExercisesApplicationTests.java
@@ -0,0 +1,13 @@
+package se.kalmar.adam.MVCExercises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcExercisesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
